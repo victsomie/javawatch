d13f5ec147bdce74a078c79cc09fce84ff7ea8ab
victsomie <victsomie@yahoo.com>
2016-04-14 23:00:14 +0300

    added more content

---
+++ StringsAndIntegers.java
@@ -0,0 +1,25 @@
+package StringsAndIntegers;
+
+public class StringsAndIntegers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str = "I am awesome"; //Declares a variable str with data type String 
+		
+		String str2 = "59000";
+		int num = 12345; //Declares 
+		int num2 
+		
+		int 
+		
+		
+		System.out.println(str); //Prints the string
+		System.out.println(num); //Prints the num
+		System.out.println(str2); //Prints the num
+		
+	}
+
+}
