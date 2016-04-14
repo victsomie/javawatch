8df1b4651f90a601ddb2b55e802a687ead587df7
victsomie <victsomie@yahoo.com>
2016-04-15 00:00:26 +0300

    added scanner

--- StringsAndIntegers.java
+++ StringsAndIntegers.java
@@ -1,4 +1,5 @@
 package StringsAndIntegers;
+import java.util.Scanner;
 
 public class StringsAndIntegers {
 
@@ -8,17 +9,32 @@ public class StringsAndIntegers {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String str = "I am awesome"; //Declares a variable str with data type String 
-		
+		int num = 12345; //Declares an integer
 		String str2 = "59000";
-		int num = 12345; //Declares 
-		int num2 
 		
-		int 
 		
+		int num2 = Integer.parseInt(str2); //Converts the string to integer
 		
 		System.out.println(str); //Prints the string
 		System.out.println(num); //Prints the num
-		System.out.println(str2); //Prints the num
+		System.out.println("The string it converted to number : >> " + num2); 
+		//Prints the converted number
+		System.out.println("Now adds the converted number: >> " + (num + num2)); //now you can add this number
+		
+		System.out.println("HOW TO REVERSE A STRING"); 
+		
+		//Declare two string 
+		//(One empty and another initialized to an empty string to hold empty )
+		
+		String x, y= "";
+		//Import a scanner to allow input!
+		Scanner sc = new Scanner(System.in);
+	
+		
+		
+		
+		
+		
 		
 	}
 
